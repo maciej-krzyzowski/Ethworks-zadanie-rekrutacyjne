eede2f0676669e9570dd55f8f616be7bef65512a
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2020-10-22 21:29:32 +0200

    final version

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const body = document.querySelector('body');
-const navItems = document.querySelectorAll(".nav__link");
-const activeItem = document.querySelector(".nav__link--active");
+const navLinks = document.querySelectorAll(".nav__link");
 const modal = document.querySelector(".modal");
 const modalChildren = document.querySelector(".modal__children");
 const closeModalBtn = document.querySelector(".modal__close");
@@ -12,22 +11,7 @@ const sectionContact = document.getElementById('contact');
 const halfWindowHeight = window.innerHeight * 0.5;
 const slider = document.querySelector('.team__slider');
 const dots = document.querySelectorAll('.slider__dot');
-
-const navScroll = () => {
-    const scrollWindow = window.scrollY;
-
-    for(let item of navItems) {
-        item.classList.remove('nav__link--active');
-    };
-    
-    if(scrollWindow < sectionHeader.offsetHeight - halfWindowHeight) {
-        navItems[0].classList.add('nav__link--active');
-    } else if (scrollWindow >= sectionTeam.offsetTop - halfWindowHeight && scrollWindow < sectionContact.offsetTop - halfWindowHeight) {
-        navItems[1].classList.add('nav__link--active');
-    } else if (scrollWindow >= sectionContact.offsetTop - halfWindowHeight){
-        navItems[2].classList.add('nav__link--active');
-    };
-};
+const offsetModifier = -70;
 
 const activateDot1 = () => {
     dots[1].classList.remove('slider__dot--active');
@@ -50,15 +34,47 @@ const closeModal = () => {
     modalChildren.innerHTML = '';
 }
 
-const action = () => {
-    for(let item of navItems) {
-        item.addEventListener("click", () => {
-            activeItem.classList.remove('nav__link--active');
-            item.classList.add('nav__link--active');
-        });
+const removeActiveLink = () => {
+    for(let item of navLinks) {
+        item.classList.remove('nav__link--active');
     };
+}
 
-    window.addEventListener('scroll', navScroll);
+const action = () => {
+
+    navLinks.forEach(navLink => {
+      navLink.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const activeLink = document.querySelector('.nav__link--active')
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        const calculatedOffset = targetElement.offsetTop + offsetModifier;
+
+        activeLink.classList.remove('nav__link--active');
+        navLink.classList.add('nav__link--active');
+
+        window.scrollTo({
+            top: calculatedOffset,
+            behavior: 'smooth'
+        });
+      });
+    });
+
+    window.addEventListener('scroll', () => {
+        const scrollWindow = window.scrollY;
+        const activeLink = document.querySelector('.nav__link--active')
+    
+        if(scrollWindow < sectionHeader.offsetHeight - halfWindowHeight && !navLinks[0].classList.contains('nav__link--active')) {
+            activeLink.classList.remove('nav__link--active');
+            navLinks[0].classList.add('nav__link--active');
+        } else if (scrollWindow >= sectionTeam.offsetTop - halfWindowHeight && scrollWindow < sectionContact.offsetTop - halfWindowHeight && !navLinks[1].classList.contains('nav__link--active')) {
+            activeLink.classList.remove('nav__link--active');
+            navLinks[1].classList.add('nav__link--active');
+        } else if (scrollWindow >= sectionContact.offsetTop - halfWindowHeight  && !navLinks[2].classList.contains('nav__link--active')){
+            activeLink.classList.remove('nav__link--active');
+            navLinks[2].classList.add('nav__link--active');
+        };
+    });
 
     videoBtn.addEventListener('click', () => {
         modalChildren.innerHTML += '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/BHACKCNDMW8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
